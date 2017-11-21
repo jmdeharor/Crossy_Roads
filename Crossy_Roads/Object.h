d0d4a6c78d9d2878048b282862f8def75329919b
EnderRL <carlosroldanmontaner@gmail.com>
2017-11-21 22:08:39 +0100

    Implemented basic movement

--- Crossy_Roads/Object.h
+++ Crossy_Roads/Object.h
@@ -18,6 +18,7 @@ public:
 	void update(int deltaTime);
 	void setPos(glm::vec3 pos);
 	void move(float x, float y, float z);
+	void move(glm::vec3 movement);
 	void rotateX(float angle);
 	void rotateY(float angle);
 	void rotateZ(float angle);
